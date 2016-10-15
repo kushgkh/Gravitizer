2aec5d78cc23bf49e49b99b480d1da03bdcf7bcb
Kush Khanolkar <kushgk@pioneer-inc.com>
2016-10-15 12:01:22 -0700

    added camera rotation and other stuff

--- Drop The Ball/Assets/Gravity.cs
+++ Drop The Ball/Assets/Gravity.cs
@@ -12,6 +12,10 @@ public class Gravity : MonoBehaviour {
 	public Camera cam;
 	Vector3 vec;
 	public bool reset = false;
+	bool spinning = false;
+	float initAngle = 0;
+	float gravityAngle;
+	public static bool Suck = false;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -26,53 +30,79 @@ public class Gravity : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			reset = true;
 		}
+		if (!isShot) {
+			if (Input.GetMouseButtonDown (0)) {
 
-		if (Input.GetMouseButtonDown (0)) {
+				arrow.SetActive (true);
+				//pInit = new Vector3 (470, 265, 0);
+				pInit = Input.mousePosition;
+
+
+				vec = cam.ScreenToWorldPoint (pInit);
+				Debug.Log (vec);
+				arrow.transform.position = vec;
+				//Debug.Log (Input.mousePosition);
+			}
+			if (Input.GetMouseButton (0)) {
+				pFinal = Input.mousePosition;
+				//arrow.transform.localScale = new Vector3 (Vector3.Distance (pInit, pFinal)/2, 5, 1);
+				Rect arrowRect = new Rect(0,0,0,Vector3.Distance (pInit, pFinal));
+				arrow.GetComponent<RectTransform> ().sizeDelta = new Vector2 (227f, Vector3.Distance (pInit, pFinal) * 10f);
+				arrow.transform.position = new Vector3 (vec.x, vec.y, 0);
+
+				float angle = Mathf.Atan ((pFinal.y - pInit.y) / (pFinal.x - pInit.x));
+				angle = angle * 180 / Mathf.PI;
+
+				if (pFinal.x - pInit.x < 0) {
+					angle += 180;
+				} 
+				gravityAngle = angle;
+				//Debug.Log (angle);
+				arrow.transform.eulerAngles = new Vector3(0,0 ,angle + 90);
+
+
+
+			}
+			if (Input.GetMouseButtonUp (0)) {
+				
+
+
+
+
+				spinning = true;
+			}
 			
-			arrow.SetActive (true);
-			//pInit = new Vector3 (470, 265, 0);
-			pInit = Input.mousePosition;
-
-
-			vec = cam.ScreenToWorldPoint (pInit);
-			Debug.Log (vec);
-			arrow.transform.position = vec;
-			//Debug.Log (Input.mousePosition);
-		}
-		if (Input.GetMouseButton (0)) {
-			pFinal = Input.mousePosition;
-			//arrow.transform.localScale = new Vector3 (Vector3.Distance (pInit, pFinal)/2, 5, 1);
-			Rect arrowRect = new Rect(0,0,0,Vector3.Distance (pInit, pFinal));
-			arrow.GetComponent<RectTransform> ().sizeDelta = new Vector2 (227f, Vector3.Distance (pInit, pFinal) * 10f);
-			arrow.transform.position = new Vector3 (vec.x, vec.y, 0);
-
-			float angle = Mathf.Atan ((pFinal.y - pInit.y) / (pFinal.x - pInit.x));
-			angle = angle * 180 / Mathf.PI;
-
-			if (pFinal.x - pInit.x < 0) {
-				angle += 180;
-			} 
-			//Debug.Log (angle);
-			arrow.transform.eulerAngles = new Vector3(0,0 ,angle + 90);
-
-
-
-		}
-		if (Input.GetMouseButtonUp (0)) {
-			rb.velocity = new Vector3 (5, 5, 0);
-
-			isShot = true;
 		}
 
-		if (isShot) {
+		if (spinning) {
+			float deltaAngle = ((gravityAngle + 90) - 0) * Time.deltaTime;
+			
+			cam.transform.eulerAngles= new Vector3(0,0 ,cam.transform.eulerAngles.z + deltaAngle);
+		
+
+			if (Mathf.Abs(cam.transform.eulerAngles.z - gravityAngle - 90) < 2) {
+				rb.velocity = new Vector3 (5, 5, 0);
+				spinning = false;
+				isShot = true;
+			}
+		}
+		//Debug.Log (Suck);
+
+		if (isShot && !Suck) {
 			rb.AddForce ((pFinal - pInit) * 0.03f);
+		}
+		if (Suck) {
+			rb.velocity = Vector3.zero;
+			
 		}
 
 		if (reset) {
+			Suck = false;
 			arrow.transform.position = new Vector3 (0, 0, 0);
 			arrow.GetComponent<RectTransform> ().sizeDelta = new Vector2 (0, 0);
 			transform.position = ballPInit;
 			rb.velocity = Vector3.zero;
+			cam.transform.eulerAngles = Vector3.zero;
 			isShot = false;
 			reset = false;
 		}
---
+++ Drop The Ball/Assets/Suction.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suction : MonoBehaviour {
+
+	public GameObject ball;
+	public float d;
+	Vector3 initPos;
+	public GameObject explosion;
+
+	float t=0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Vector3.Distance (ball.transform.position, transform.position) < d) {
+			if (!Gravity.Suck) {
+				initPos = ball.transform.position;
+			}
+			Gravity.Suck = true;
+			//Debug.Log (Vector3.Distance (ball.transform.position, transform.position));
+			if (t < 1) {
+				ball.transform.position = Vector3.Lerp (initPos, transform.position, t);
+				t +=  2 * Time.deltaTime;
+			} else {
+				Debug.Log ("yo");
+				Gravity.Suck = false;
+				explosion.SetActive (true);
+				ball.transform.position = new Vector3 (-100, -100, 50);
+				t = 0;
+			}
+		}
+		if (Input.GetKeyDown ("space")) {
+			explosion.SetActive (false);
+			
+		}
+
+			
+
+	}
+}
